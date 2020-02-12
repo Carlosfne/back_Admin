8984eb088bb157199a8664f87d565222deee2038
unknown <carlos.junior@grupoicts.com.br>
2020-02-12 10:50:09 -0400

    Aws e S3 implementado

--- server.js
+++ server.js
@@ -1,3 +1,5 @@
+require('dotenv').config();
+
 const express = require('express'),
       cors = require('cors'),
       mongoose = require('mongoose'),
--- src/config/multer.js
+++ src/config/multer.js
@@ -1,10 +1,10 @@
 const multer = require('multer'),
       path = require('path'),
-      crypto = require('crypto');
+      crypto = require('crypto'),
+      multerS3 = require('multer-s3');
 
-module.exports = {
-  dest: path.resolve(__dirname, '..', '..', 'tmp', 'uploads'),
-  storage: multer.diskStorage({
+const storageTypes = {
+  local: multer.diskStorage({
     destination: ( req, file, cb) =>{
       cb( null, path.resolve(__dirname, '..', '..', 'tmp', 'uploads'))
     },
@@ -18,6 +18,26 @@ module.exports = {
       })
     }
   }),
+  s3: multerS3({
+    s3: new aws.S3(),
+    bucket:'paineladmin',
+    contentType: multerS3.AUTO_CONTENT_TYPE,
+    acl: 'public-read',
+    key: (req, file, cb) =>{
+      crypto.randomBytes(16, (err, hash) => {
+        if (err) cb(err);
+
+        const fileName = `${hash.toString('hex')}-${file.originalname}`;
+
+        cb(null, fileName);
+      })
+    }
+  })
+}
+
+module.exports = {
+  dest: path.resolve(__dirname, '..', '..', 'tmp', 'uploads'),
+  storage: storageTypes['s3'],
   limits: {
     fileSize: 2 * 1024 * 1024
   },
