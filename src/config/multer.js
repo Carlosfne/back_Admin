51f1279a6b4d87423dd247336ff2963986e5b0b1
unknown <carlos.junior@grupoicts.com.br>
2020-02-12 10:53:04 -0400

    s3

--- src/config/multer.js
+++ src/config/multer.js
@@ -37,7 +37,7 @@ const storageTypes = {
 
 module.exports = {
   dest: path.resolve(__dirname, '..', '..', 'tmp', 'uploads'),
-  storage: storageTypes['s3'],
+  storage: storageTypes["s3"],
   limits: {
     fileSize: 2 * 1024 * 1024
   },
